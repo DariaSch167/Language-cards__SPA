0b781680a95e4900739e5ff171685348031b3d71
Daria <scheglova.daria@gmail.com>
2024-06-03 01:19:42 +0300

    add wp-configs up to dist delete

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "template.html"),
+      filename: "index.html",
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: [
+            {
+              source: path.join(__dirname, "dist").replaceAll("\\", "/"),
+              options: {
+                force: true,
+                recursive: true,
+              },
+            },
+          ],
+        },
+      },
+    }),
+  ],
+  devServer: {
+    watchFiles: path.join(__dirname, "src"),
+    port: 9000,
+  },
+};
