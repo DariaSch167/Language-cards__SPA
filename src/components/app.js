f96408aa19b4000b6ab219f9ab680a54e0e3bd8c
Daria <scheglova.daria@gmail.com>
2024-06-04 01:36:06 +0300

    test jsx

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
+import Header from "./header";
 
 class App extends Component {
   render() {
     return (
-      <>
-        <h1 className="h1">Приложение на React</h1>
-      </>
+      <Header />
+      /* <h1 className="h1">Приложение на React</h1> */
     );
   }
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h2>Hello hello!!!</h2>
+    </React.Fragment>
+  );
+}
+export default Header;
